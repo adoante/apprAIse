674323fe0a2e7a5088ab3d68bfa56e7f1cf1d545
Adolfo Gante <adolfogante@gmail.com>
2025-03-05 22:30:10 -0800

    add: get_customization()

--- database/sql_helper.py
+++ database/sql_helper.py
@@ -1,9 +1,16 @@
 from database.database_init import *
 from sqlmodel import Session, select
 
-# Get all data on specific user by USER_ID
+# Get all data from specific user by USER_ID
 def get_user(user_id: int):
 	with Session(engine) as session:
 		statement = select(User).where(User.user_id == user_id)
 		user = session.exec(statement).first()
 	return user
+
+# Get all data from specific customization  by CUSTOMIZATION_ID
+def get_customization(customization_id: int):
+	with Session(engine) as session:
+		statement = select(Customization).where(Customization.customization_id == customization_id)
+		customization = session.exec(statement).first()
+	return customization
