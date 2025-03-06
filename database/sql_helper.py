979f0c134161714764be14416d619bc8408d0995
Adolfo Gante <adolfogante@gmail.com>
2025-03-05 22:08:05 -0800

    removed test

--- database/sql_helper.py
+++ database/sql_helper.py
@@ -1,25 +1,9 @@
-from database.database_init import engine, User
+from database.database_init import *
 from sqlmodel import Session, select
 
+# Get all data on specific user by USER_ID
 def get_user(user_id: int):
 	with Session(engine) as session:
 		statement = select(User).where(User.user_id == user_id)
 		user = session.exec(statement).first()
 	return user
-
-# Tests
-
-with Session(engine) as session:
-	user = User(
-		user_id=0,
-		user_name="testUser",
-		first_name="test",
-		last_name="user",
-		email="test@user.com",
-		password_hash="hashed-password",
-		qai_hub_api_token="qai-api-token",
-		customization_id=None)
-	session.add(user)
-	session.commit()
-
-print(get_user(0))
