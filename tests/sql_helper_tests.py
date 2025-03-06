82393965c36b987132a9d26a1b8a47724b4a2d36
Adolfo Gante <adolfogante@gmail.com>
2025-03-05 22:41:39 -0800

    created test folder and moved sql_helper_tests.py

---
+++ tests/sql_helper_tests.py
@@ -0,0 +1,26 @@
+from database.sql_helper import *
+
+# Test get_user()
+# with Session(engine) as session:
+# 	user = User(
+# 		user_id=0,
+# 		user_name="testUser",
+# 		first_name="test",
+# 		last_name="user",
+# 		email="test@user.com",
+# 		password_hash="hashed-password",
+# 		qai_hub_api_token="qai-api-token",
+# 		customization_id=None)
+# 	session.add(user)
+# 	session.commit()
+# print(get_user(0))
+
+# Test get_customization()
+# with Session(engine) as session:
+# 	customization = Customization(customization_id=0)
+# 	session.add(customization)
+# 	session.commit()
+# print(get_customization(0))
+
+
+
