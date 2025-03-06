861fec90aa2e0171b597b30bb5a4d5b0a1bbec17
Adolfo Gante <adolfogante@gmail.com>
2025-03-05 21:37:07 -0800

    added test for get_user()

--- database/sql_helper.py
+++ database/sql_helper.py
@@ -1,4 +1,4 @@
-from database.database_init import *
+from database.database_init import engine, User
 from sqlmodel import Session, select
 
 def get_user(user_id: int):
@@ -6,3 +6,20 @@ def get_user(user_id: int):
 		statement = select(User).where(User.user_id == user_id)
 		user = session.exec(statement).first()
 	return user
+
+# Tests
+
+with Session(engine) as session:
+	user = User(
+		user_id=0,
+		user_name="testUser",
+		first_name="test",
+		last_name="user",
+		email="test@user.com",
+		password_hash="hashed-password",
+		qai_hub_api_token="qai-api-token",
+		customization_id=None)
+	session.add(user)
+	session.commit()
+
+print(get_user(0))
