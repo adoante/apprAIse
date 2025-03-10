b68d1e15a340e547a2cfbd7bf77a631bcea087c6
Adolfo Gante <adolfogante@gmail.com>
2025-03-10 16:39:38 -0700

    read user by id using a form

--- frontend/js/api_wrapper.js
+++ frontend/js/api_wrapper.js
@@ -18,25 +18,25 @@ function get_root() {
 		});
 }
 
-// read_hero(name: str)
+// read_user(user_id: str)
 document.addEventListener("DOMContentLoaded", () => {
-    const heroForm = document.getElementById("heroForm");
+    const userForm = document.getElementById("userForm");
 
-    if (heroForm) {
-        heroForm.addEventListener("submit", read_hero);
+    if (userForm) {
+        userForm.addEventListener("submit", read_user);
     }
 });
 
-function read_hero(event) {
+function read_user(event) {
     event.preventDefault();
-    const heroForm = event.target;
-    const heroFormData = new FormData(heroForm);
+    const userForm = event.target;
+    const userFormData = new FormData(userForm);
 
-    heroName = Array.from(heroFormData.entries())[0][1];
+    userID = Array.from(userFormData.entries())[0][1];
 
-	const outputElement = document.getElementById("test-read-hero");
+	const outputElement = document.getElementById("test-read-user");
 
-	fetch(`http://127.0.0.1:8000/hero/${heroName}`)
+	fetch(`http://127.0.0.1:8000/user/${userID}`)
 		.then(response => {
 			if (!response.ok) {
 				throw new Error('API request failed.')
@@ -49,6 +49,6 @@ function read_hero(event) {
 		})
 		.catch(error => {
 			console.error(error)
-			outputElement.innerHTML = `'${heroName}' not found.`
+			outputElement.innerHTML = `'${userID}' not found.`
 		});
 }
