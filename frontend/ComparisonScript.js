11cb0ce521e9e4db4136b368dba83d20faa345df
Adolfo Gante <adolfogante@gmail.com>
2025-03-25 05:50:19 -0700

    charts section complete with 4 charts

--- frontend/ComparisonScript.js
+++ frontend/ComparisonScript.js
@@ -74,28 +74,204 @@ document.addEventListener("DOMContentLoaded", function () {
 		})
 	})
 
-	/* On click update respective model column data*/
-    document.querySelectorAll(".dropdown-content-model").forEach(button => {
-		button.addEventListener("click", function (event) {
-			if (event.target.classList.contains("dropdownItem")) {
-				console.log(event.target.textContent + " clicked!");
-			}
-		})
-	})
+	const model_names = []
 
-	document.querySelectorAll(".dropdown-content-device").forEach(button => {
-		button.addEventListener("click", function (event) {
+	function getModelNames(id, idx) {
+		document.getElementById(id).addEventListener("click", function (event) {
 			if (event.target.classList.contains("dropdownItem")) {
 				console.log(event.target.textContent + " clicked!");
+				model_names[idx] = event.target.textContent
+				console.log(model_names)
 			}
-		})
-	})
+		});
+	}
 
-	document.querySelectorAll(".dropdown-content-library").forEach(button => {
-		button.addEventListener("click", function (event) {
+	getModelNames("dropdown-content-model-1", 0)
+	getModelNames("dropdown-content-model-2", 1)
+	getModelNames("dropdown-content-model-3", 2)
+
+	const device_names = []
+
+	function getDeviceNames(id, idx) {
+		document.getElementById(id).addEventListener("click", function (event) {
 			if (event.target.classList.contains("dropdownItem")) {
 				console.log(event.target.textContent + " clicked!");
+				device_names[idx] = event.target.textContent
+				console.log(device_names)
+				getBenchmarks()
 			}
-		})
-	})
+		});
+	}
+
+	getDeviceNames("dropdown-content-device-1", 0)
+	getDeviceNames("dropdown-content-device-2", 1)
+	getDeviceNames("dropdown-content-device-3", 2)
+
+	const library_names = []
+
+	function getLibraryNames(id, idx) {
+		document.getElementById(id).addEventListener("click", function (event) {
+			if (event.target.classList.contains("dropdownItem")) {
+				console.log(event.target.textContent + " clicked!");
+				library_names[idx] = event.target.textContent
+				console.log(library_names)
+				getBenchmarks()
+			}
+		});
+	}
+
+	getLibraryNames("dropdown-content-library-1", 0)
+	getLibraryNames("dropdown-content-library-2", 1)
+	getLibraryNames("dropdown-content-library-3", 2)
+
+	const accuracy_top1_scores = {}
+	const accuracy_top5_scores = {}
+	const inference_times = {}
+	const memory_usage = {}
+
+	function getBenchmarks() {
+		if (model_names.length != 0) {
+			if (library_names.length == device_names.length) {
+				for (let i = 0; i < library_names.length; i++) {
+					if (library_names[i] != "" && device_names[i] != "") {
+						api.filter_benchmarks(device_names[i], library_names[i])
+							.then(benchmarkData => {
+								benchmarkData["benchmarks"].forEach(benchmark => {
+									api.read_model(benchmark["model_id"]).then(
+										modelData => {
+											if (model_names.includes(modelData["model_name"])) {
+												accuracy_top1_scores[modelData["model_name"]] = benchmark["accuracy_top1"]
+												accuracy_top5_scores[modelData["model_name"]] = benchmark["accuracy_top5"]
+												inference_times[modelData["model_name"]] = benchmark["inference_time"]
+												memory_usage[modelData["model_name"]] = benchmark["memory_usage"]
+											}
+										}
+									)
+								})
+							})
+					}
+				}
+			}
+		}
+	}
+
+	let accuracyTop1Chart;
+	let accuracyTop5Chart;
+	let inferenceTimeChart;
+	let memoryUsageChart;
+
+	function constructAccuracyTop1Chart() {
+		if (accuracyTop1Chart) {
+			accuracyTop1Chart.destroy();
+		}
+
+		const ctx = document.getElementById('accuracyTop1Chart');
+
+		accuracyTop1Chart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: Object.keys(accuracy_top1_scores),
+				datasets: [{
+					label: 'Accuracy Top 1',
+					data: Object.values(accuracy_top1_scores),
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	}
+
+	function constructAccuracyTop5Chart() {
+		if (accuracyTop5Chart) {
+			accuracyTop5Chart.destroy();
+		}
+
+		const ctx = document.getElementById('accuracyTop5Chart');
+
+		accuracyTop5Chart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: Object.keys(accuracy_top5_scores),
+				datasets: [{
+					label: 'Accuracy Top 5',
+					data: Object.values(accuracy_top5_scores),
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	}
+
+	function constructInferenceTimeChart() {
+		if (inferenceTimeChart) {
+			inferenceTimeChart.destroy();
+		}
+
+		const ctx = document.getElementById('inferenceTimeChart');
+
+		inferenceTimeChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: Object.keys(inference_times),
+				datasets: [{
+					label: 'Inference Time',
+					data: Object.values(inference_times),
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	}
+
+	function constructMemoryUsageChart() {
+		if (memoryUsageChart) {
+			memoryUsageChart.destroy();
+		}
+
+		const ctx = document.getElementById('memoryUsageChart');
+
+		memoryUsageChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: Object.keys(memory_usage),
+				datasets: [{
+					label: 'Accuracy Top 1',
+					data: Object.values(memory_usage),
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	}
+
+	document.querySelector(".ChartBtn").addEventListener("click", function (event) {
+		constructAccuracyTop1Chart()
+		constructAccuracyTop5Chart()
+		constructInferenceTimeChart()
+		constructMemoryUsageChart()
+	});
 });
