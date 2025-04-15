f89b55e3a1f3504a464bb7688c34559d6e8e4b2f
Adolfo Gante <adolfogante@gmail.com>
2025-04-14 19:48:12 -0700

    api serves frontend now so baseURL needs to change

--- frontend/js/modules/api_wrapper.js
+++ frontend/js/modules/api_wrapper.js
@@ -19,7 +19,7 @@ https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
 
 */
 
-const baseURL = "http://127.0.0.1:8000/api/v1"
+const baseURL = "/api/v1";
 
 async function fetchData(endpoint, id = "") {
     try {
