56607a29d45390c6bc1f41ef150d3079e03c0774
JustinasJanovskis <164398237+JustinasJanovskis@users.noreply.github.com>
2025-04-16 16:42:08 -0700

    uploadImage requirements update

---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from .endpoints import user, customization, favorite, chipset, model, device, benchmark, library, auth, inference
+
+router = APIRouter()
+
+router.include_router(user.router, prefix = "/user", tags = ["user"])
+router.include_router(customization.router, prefix = "/customization", tags = ["customization"])
+router.include_router(favorite.router, prefix = "/favorite", tags = ["favorite"])
+router.include_router(chipset.router, prefix = "/chipset", tags = ["chipset"])
+router.include_router(device.router, prefix = "/device", tags = ["device"])
+router.include_router(model.router, prefix = "/model", tags = ["model"])
+router.include_router(benchmark.router, prefix = "/benchmark", tags = ["benchmark"])
+router.include_router(auth.router, prefix="/auth", tags = ["auth"])
+router.include_router(library.router, prefix = "/library", tags = ["library"])
+router.include_router(inference.router, prefix = "/inference", tags = ["inference"])
