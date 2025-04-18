99865136f5830336a0a1a9e723f6c50555a64158
Adolfo Gante <adolfogante@gmail.com>
2025-04-17 21:38:37 -0700

    Inference page gets added to navbar when loged in

--- frontend/CommonScript.js
+++ frontend/CommonScript.js
@@ -11,15 +11,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const access_token = localStorage.getItem("access_token")
 	if (access_token) {
+
 		loginLink.href = "Dashboard.html"
 		loginLink.textContent = "Dashboard"
 
 		if (loginLinkFooter.lastElementChild) {
 			loginLinkFooter.removeChild(loginLinkFooter.lastElementChild)
+			
+			loginLinkFooter.insertAdjacentHTML(
+				"beforeend",
+				`<li><a class="footerMenuItem" href="uploadImage.html">Inference</a></li>`
+			)
+
 			loginLinkFooter.insertAdjacentHTML(
 				"beforeend",
 				`<li><a class="footerMenuItem" href="Dashboard.html">Dashboard</a></li>`
 			)
 		}
+
+		loginLink.insertAdjacentHTML(
+			"beforebegin",
+			`<li><a class="navMenu" href="uploadImage.html">Inference</a></li>`
+		)
 	}
 });
